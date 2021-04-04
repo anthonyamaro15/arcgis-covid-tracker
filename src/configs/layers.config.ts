5f073b43b1f0b48af833a00b20d1bd28a785f67b
Anthony Amaro <aamaro@bradmins-MacBook-Pro.local>
2021-04-03 18:19:39 -0700

    feat(map-controller): create new visual variables

--- src/configs/layers.config.ts
+++ src/configs/layers.config.ts
@@ -1,15 +1,61 @@
 import { trackerPopup } from "../helperFiles";
 
+const color = {
+   type: "color",
+   field: "Deaths",
+   legendOptions: {
+      title: "number of death by color",
+      showLegend: true,
+   },
+   stops: [
+      { value: 10, color: "black", label: "test" },
+      { value: 1000, color: "red", label: "test" },
+      { value: 2000, color: "purple", label: "test" },
+      { value: 5000, color: "black", label: "test" },
+      { value: 10000, color: "green", label: "test" },
+   ],
+};
+
+const size = {
+   type: "size",
+   field: "Deaths",
+   valueExpression: "$feature.Deaths",
+   legendOptions: {
+      title: "number of deaths by size",
+   },
+   stops: [
+      { value: 10, size: 9, label: "100" },
+      { value: 1000, size: 8, label: "1000" },
+      { value: 2000, size: 16, label: "2000" },
+      { value: 5000, size: 20, label: "5000" },
+      { value: 10000, size: 25, label: "10000" },
+   ],
+};
+
+const renderer = {
+   type: "simple",
+   symbol: {
+      type: "simple-marker",
+      outline: {
+         width: 1,
+         color: "white",
+      },
+   },
+   visualVariables: [color, size],
+};
+
 export const layers = [
    {
-      type: 'FeatureLayer',
+      type: "FeatureLayer",
       options: {
-         url: "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases2_v1/FeatureServer/1",
+         url:
+            "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases2_v1/FeatureServer/1",
          id: "covid-tracker",
          title: "covid tracker",
          visible: true,
          outFields: ["*"],
-         popupTemplate: trackerPopup
-      }
-   }
+         popupTemplate: trackerPopup,
+         renderer,
+      },
+   },
 ];
--- src/controllers/MapController.tsx
+++ src/controllers/MapController.tsx
@@ -1,17 +1,17 @@
-import { RefObject } from 'react';
-import Map from '@arcgis/core/Map';
-import MapView from '@arcgis/core/views/MapView';
-import config from '../configs/config';
-import layerFactory from '../utils/layerFactory.utils';
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
-import BaseMapGallery from '@arcgis/core/widgets/BasemapGallery';
-import Expand from '@arcgis/core/widgets/Expand';
-import Search from '@arcgis/core/widgets/Search';
-import FeatureLayerView from '@arcgis/core/views/layers/FeatureLayerView';
-import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
-import Sketch from '@arcgis/core/widgets/Sketch';
-import store from '../redux/store';
-import { setMapLoaded } from '../redux/slices/mapSlice';
+import { RefObject } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import config from "../configs/config";
+import layerFactory from "../utils/layerFactory.utils";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import BaseMapGallery from "@arcgis/core/widgets/BasemapGallery";
+import Expand from "@arcgis/core/widgets/Expand";
+import Search from "@arcgis/core/widgets/Search";
+import FeatureLayerView from "@arcgis/core/views/layers/FeatureLayerView";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Sketch from "@arcgis/core/widgets/Sketch";
+import store from "../redux/store";
+import { setMapLoaded } from "../redux/slices/mapSlice";
 
 class MapController {
    #map?: Map;
@@ -23,44 +23,52 @@ class MapController {
    #layer?: GraphicsLayer;
    #sketch?: Sketch;
    layerView?: FeatureLayerView | any;
-   
+
    initializeMap = async (domRef: RefObject<HTMLDivElement>) => {
-      if(!domRef.current) {
+      if (!domRef.current) {
          return;
       }
 
       this.#layer = new GraphicsLayer();
 
       this.#map = new Map({
-         basemap: "hybrid",
-         layers: [this.#layer]
+         basemap: "gray",
+         layers: [this.#layer],
       });
 
       this.#mapview = new MapView({
          map: this.#map,
          container: domRef.current,
          center: [-100.33, 25.69],
-         zoom: 5
+         zoom: 5,
       });
 
-      this.#baseMapGallery = new BaseMapGallery({ view: this.mapView, container: document.createElement('div') });
+      this.#baseMapGallery = new BaseMapGallery({
+         view: this.mapView,
+         container: document.createElement("div"),
+      });
       this.#bgExpand = new Expand({
-         view: this.#mapview, content: this.#baseMapGallery,
+         view: this.#mapview,
+         content: this.#baseMapGallery,
       });
 
       this.#searchWidget = new Search({ view: this.#mapview });
-      this.#sketch = new Sketch({ layer: this.#layer, view: this.#mapview, creationMode: 'update' });
+      this.#sketch = new Sketch({
+         layer: this.#layer,
+         view: this.#mapview,
+         creationMode: "update",
+      });
 
-      this.#mapview.ui.add(this.#sketch, 'bottom-right');
-      this.#mapview.ui.add(this.#searchWidget, 'top-right');
-      this.#mapview.ui.add(this.#bgExpand, 'top-right');
+      this.#mapview.ui.add(this.#sketch, "bottom-right");
+      this.#mapview.ui.add(this.#searchWidget, "top-right");
+      this.#mapview.ui.add(this.#bgExpand, "top-right");
 
       this.#mapview?.when(async () => {
          this.#mapLayers = [];
-         
-         for(let i = 0; i < config.layers.length; i++) {
+
+         for (let i = 0; i < config.layers.length; i++) {
             const layer = await layerFactory(config.layers[i]);
-            if(layer) {
+            if (layer) {
                this.#mapLayers.push(layer);
                this.#map?.add(layer);
             }
@@ -69,43 +77,45 @@ class MapController {
          // const nodes = document.querySelectorAll(".testing");
          const node = document.getElementById("dead-filter");
 
-         node?.addEventListener('click', (e) => this.filterByDeath(e, this.layerView))
+         node?.addEventListener("click", (e) =>
+            this.filterByDeath(e, this.layerView),
+         );
 
-         const layer = this.#map?.findLayerById('covid-tracker') as FeatureLayer;
+         const layer = this.#map?.findLayerById(
+            "covid-tracker",
+         ) as FeatureLayer;
 
-         
-        
          this.#mapview?.whenLayerView(layer)?.then((featureLayerView) => {
             this.layerView = featureLayerView;
 
-            if(node) {
-               node.style.visibility = 'visible';
+            if (node) {
+               node.style.visibility = "visible";
                const nodesExpand = new Expand({
                   view: this.#mapview,
                   content: node,
-                  expandIconClass: 'esri-icon-filter',
-                  group: 'top-left'
+                  expandIconClass: "esri-icon-filter",
+                  group: "top-left",
                });
 
-               nodesExpand.watch('expanded', () => {
-                  if(!nodesExpand.expanded) {
+               nodesExpand.watch("expanded", () => {
+                  if (!nodesExpand.expanded) {
                      this.layerView.filter = null;
                   }
                });
-               this.#mapview?.ui.add(nodesExpand, 'top-left');
+               this.#mapview?.ui.add(nodesExpand, "top-left");
             }
          });
          store.dispatch(setMapLoaded(true));
-      })
-   }
+      });
+   };
 
    // cast types as any for the moment
-   filterByDeath(e: any, layerView: any) {
-      const selectedNode = e.target.getAttribute('data-count');
+   filterByDeath = (e: any, layerView: any) => {
+      const selectedNode = e.target.getAttribute("data-count");
       layerView.filter = {
-         where: "Country_Region = '" + selectedNode + "'" 
-      }
-   }
+         where: "Country_Region = '" + selectedNode + "'",
+      };
+   };
 
    get map() {
       return this.#map;
@@ -115,7 +125,7 @@ class MapController {
    }
    get mapLayer() {
       return this.#mapLayers;
-   } 
+   }
 }
 
 const mapController = new MapController();
@@ -125,7 +135,7 @@ declare global {
       mapController: typeof mapController;
    }
 }
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
    window.mapController = mapController;
 }
 
