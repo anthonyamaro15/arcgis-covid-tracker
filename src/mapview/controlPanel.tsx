31916504c6f3776931c9080aec9db814772bf8ba
Anthony Amaro <aamaro@bradmins-MacBook-Pro.local>
2021-04-03 19:46:34 -0700

    feat(map-controller): find a way to get all countries from data

--- src/mapview/controlPanel.tsx
+++ src/mapview/controlPanel.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import mapController from "../controllers/MapController";
+
 const ControlPanel = () => {
+   const [filterBy, setFilterBy] = useState("");
+
+   useEffect(() => {
+      mapController.filterByDeath(filterBy);
+   }, [filterBy]);
    return (
       <div id="dead-filter" className="esri-widget">
-         <div className="testing visible-testing" data-count="US">US</div>
-         <div className="testing visible-testing" data-count="Venezuela">Venezuela</div>
-         <div className="testing visible-testing" data-count="Zambia">Zambia</div>
-      </div> 
-   )
-}
+         <div
+            className="testing visible-testing"
+            onClick={() => setFilterBy("US")}
+            data-count="US"
+         >
+            US
+         </div>
+         <div
+            className="testing visible-testing"
+            onClick={() => setFilterBy("Venezuela")}
+            data-count="Venezuela"
+         >
+            Venezuela
+         </div>
+         <div
+            className="testing visible-testing"
+            onClick={() => setFilterBy("Zambia")}
+            data-count="Zambia"
+         >
+            Zambia
+         </div>
+      </div>
+   );
+};
 
 export default ControlPanel;
--- src/redux/slices/mapSlice.ts
+++ src/redux/slices/mapSlice.ts
@@ -1,23 +1,31 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../store';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
-const initialState = {
-   mapLoaded: false
+interface AppState {
+   mapLoaded: boolean;
+   countries: string[];
+}
+const initialState: AppState = {
+   mapLoaded: false,
+   countries: [],
 };
 
 const mapSlice = createSlice({
-   name: 'mapSlice',
+   name: "mapSlice",
    initialState,
    reducers: {
       setMapLoaded: (state, action: PayloadAction<boolean>) => {
          state.mapLoaded = action.payload;
-      }
-   }
+      },
+      setCountries: (state, action: PayloadAction<string[]>) => {
+         state.countries = action.payload;
+      },
+   },
 });
 
-export const { setMapLoaded } = mapSlice.actions;
-
+export const { setMapLoaded, setCountries } = mapSlice.actions;
 
 export const mapLoaded = (state: RootState) => state.mapSlice.mapLoaded;
+export const countries = (state: RootState) => state.mapSlice.countries;
 
 export default mapSlice.reducer;
